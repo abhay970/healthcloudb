d28669a627e78e7edff406704d422875c7540b17
abhay <abhay.kumar.yadav@techkasetti.com>
2024-07-06 16:55:11 +0530

    add all

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+// import ComponentTest from "./predevelopments/ComponentTest";
+import Base from "./components/Base";
+
+function App() {
+  return (
+    <div className="App">
+      <Base />
+      {/* <ComponentTest /> */}
+    </div>
+  );
+}
+
+export default App;
